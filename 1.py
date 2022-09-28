82dc71ea0fb2a8423f4e91717743770b9670e5a6
super-goodman <1984814127@qq.com>
2022-09-28 17:10:32 +1300

    add one more .py file 1.py to display 123

---
+++ 1.py
@@ -0,0 +1,20 @@
+
+        
+class MatrixLed(object):
+
+    def display(self):
+        print(123)
+
+
+
+def main():
+     matrixLed = MatrixLed()
+     matrixLed.display()
+
+
+main()
+
+    
+
+
+
