ccf5aa58019babe4c1f093060e053d3ceb6f6539
Mi Zhou <denismizhou@gmail.com>
2022-10-04 14:05:12 +1300

    temp 2:05

--- SenseHat/SHTC3.py
+++ SenseHat/SHTC3.py
@@ -1,13 +1,13 @@
-
+from sense_hat import SenseHat
 class SHTC3(object): # Temp and humidity
     def __init__(self):
         self.temp = 0
         self.humi = 0  
-
+        sense=SenseHat()
     #read Tempreture
     def readTemp(self):
-        #self.temp = sensehat.readTemp()
-        print("readTemp")
+        self.temp = sense.temp
+        print(self.temp)
         return self.temp
     
     #read humidity
