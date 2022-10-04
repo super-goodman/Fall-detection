17c5cfa4dafde72b3bad069cb65894b8958ea9b1
Mi Zhou <denismizhou@gmail.com>
2022-10-04 17:51:44 +1300

    5:51

--- SenseHat/LPS22HB.py
+++ SenseHat/LPS22HB.py
@@ -1,12 +1,13 @@
-
+from sense_hat import SenseHat
 class LPS22HB(object): # Temp and humidity
     def __init__(self):
         self.pressure = 0 
-        
+        self.sense = SenseHat()
+        self.sense.clear()
     #read Tempreture
     def readPressure(self):
-        #self.temp = sensehat.readTemp()
-        print("readPressure")
+        self.pressure = self.sense.get_pressure()
+        print(self.pressure)
         return self.pressure
     
 
