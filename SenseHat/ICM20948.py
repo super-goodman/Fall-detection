374f8f7a61ae19dcfd6e047f3e210a2f10d8f128
Mi Zhou <denismizhou@gmail.com>
2022-10-04 18:08:21 +1300

    6:08

--- SenseHat/ICM20948.py
+++ SenseHat/ICM20948.py
@@ -4,23 +4,23 @@ class ICM20948(object): # Temp and humidity
         self.x = 0 
         self.y = 0 
         self.z = 0 
-        self.o = 0
+        self.acc = 0
         self.sense = SenseHat()
         self.sense.clear()
        
         
     #read Accclerometer
     def readAccclerometer(self):
-        self.o = self.sense.get_orientation()
-        self.x = self.o["x"]
-        self.y = self.o["y"]
-        self.z = self.o["z"]
-        print("x {0} y {1} z {2}".format(self.x,self.y,self.z))
+    	self.acc = self.sense.get_accelerometer_raw()
+        self.x = self.acc["x"]
+        self.y = self.acc["y"]
+        self.z = self.acc["z"]
+        self.x = round(self.x, 0)
+	    self.y = round(self.y, 0)
+	    self.z = round(self.z, 0)
+        print("x={0}, y={1}, z={2}".format(self.x,self.y,self.z))
         return self.x,self.y,self.z
     
-
-
-
 
 
 
