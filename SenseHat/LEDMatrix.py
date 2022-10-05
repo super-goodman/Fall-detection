68362fe86fe58877e3a1cce6d0bdc2d742ee6386
Mi Zhou <denismizhou@gmail.com>
2022-10-05 17:05:53 +1300

    5:05

--- SenseHat/LEDMatrix.py
+++ SenseHat/LEDMatrix.py
@@ -1,14 +1,13 @@
 from sense_hat import SenseHat
-class LEDMatrix(object): # Temp and humidity
+class LEDMatrix(object): 
     def __init__(self):
-        self.x = 0
-        self.y = 0  
+
         self.sense = SenseHat()
         self.sense.close()
         
-    #read Tempreture
+  
     def displayStr(self,str,x,y):
-        #self.temp = sensehat.readTemp()
+      
         self.sense.show_message(str, text_colour=[185, 139, 231])
         
 
