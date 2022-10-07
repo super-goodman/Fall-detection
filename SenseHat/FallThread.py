73faaada94922ad2d24f10c2c41e74cdcdca7f77
Mi Zhou <denismizhou@gmail.com>
2022-10-07 23:47:35 +1300

    11:47

--- SenseHat/FallThread.py
+++ SenseHat/FallThread.py
@@ -1,5 +1,7 @@
 import threading
 from SenseHat.ICM20948 import ICM20948
+import numpy 
+from time import sleep
 
 class FallThread (threading.Thread):
    def __init__(self, threadID, name):
@@ -9,6 +11,20 @@ class FallThread (threading.Thread):
       
    def run(self):
       print ("Starting " + self.name)
-      icm20948 = ICM20948()
+     
+      arr = numpy.empty(2400, dtype = str)
+      for i in range (2400):
+         x,y,z = ICM20948()
+         arr[i] = str(x)+','+str(y)+','+str(z)+','
+         sleep(0.005)
       print ("Exiting " + self.name)
+      
+      f = open("testData.txt", "x")
+      f.close()
+      f = open("testData.txt", "a")
+      for j in range (2400):
+          f.write(arr[j])
+      f.close()
+
+      #return arr
    
