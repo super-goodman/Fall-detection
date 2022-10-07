6d1187fe81829bc39a6664595156aed82e3803eb
Mi Zhou <denismizhou@gmail.com>
2022-10-08 00:35:49 +1300

    12:35

--- SenseHat/FallThread.py
+++ SenseHat/FallThread.py
@@ -13,17 +13,17 @@ class FallThread (threading.Thread):
       print ("Starting " + self.name)
      
       arr = numpy.empty(2400, dtype = str)
-      for i in range (2400):
+      for i in range (24):
          acc = ICM20948()
          x,y,z = acc.readAccclerometer()
          arr[i] = str(x)+','+str(y)+','+str(z)+','
          sleep(0.005)
-      print ("Exiting " + self.name + arr[150])
+      print ("Exiting " + self.name + arr[15])
       
       f1 = open("testData.txt", "x")
       f1.close()
       f2 = open("testData.txt", "a")
-      for j in range (2400):
+      for j in range (24):
           f2.write(arr[j])
       f2.close()
 
